d15fd5b5847b226bb637f4cccf2b0c86eb406c95
George <healthystake@gmail.com>
2024-06-16 19:29:50 +0000

    initial change

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string first = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name? ");
+        string last = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {first}, {first} {last}.");
     }
 }
+    
